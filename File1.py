f08141c9dd43e75d9751358e56b2365ea162786c
S <212803481@ge.com>
2021-09-28 23:00:02 +0530

    First version of code

---
+++ File1.py
@@ -0,0 +1,13 @@
+''' This is tutorial exercise to understand about _name_ = "main" '''
+print ("File1 __name__ = %s" %__name__) 
+  
+if __name__ == "__main__": 
+    print ("File1 is being run directly")
+else: 
+    print ("File1 is being imported")
+
+
+
+
+ 
+
